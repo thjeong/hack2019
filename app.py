a701c82923c647796bde119808e5d500c3c3bc54
shortranic <shortranic@gmail.com>
2019-11-19 13:33:42 +0900

    test

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__, static_url_path='', static_folder='static/dist/hack2019')
 def index():
     return send_file('dist/hack2019/index.html')
 
+@app.route('/board')
+def board_list():
+    return "GET방식 요청에 회신 : {}".format(request.args.get('question'))
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
