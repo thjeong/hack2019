44b8e539125f7eab2dc050d4300995a48155e99e
Jimmy <xikizima@hotmail.com>
2019-11-19 13:30:51 +0900

    comment out dist in gitignore

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_url_path='', static_folder='static/dist/hack2019')
 
 @app.route('/')
 def index():
-    return send_file('static/dist/hack2019/index.html')
+    return send_file('dist/hack2019/index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
