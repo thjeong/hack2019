62904f9d9d692d422e0f6fd902c79d31549d6795
Jimmy <xikizima@hotmail.com>
2019-11-19 10:16:05 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, send_file
+
+app = Flask(__name__, static_url_path='', static_folder='static/dist/hack2019')
+
+@app.route('/')
+def index():
+    return send_file('static/dist/hack2019/index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
