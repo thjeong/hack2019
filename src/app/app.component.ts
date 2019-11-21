43a8010c1aefeeac37bbec22ad290ea4cdd40245
Jimmy <xikizima@hotmail.com>
2019-11-21 16:14:45 +0900

    login page done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,17 +10,26 @@ import { User } from './_models';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  step = 0;
   currentUser: User;
 
-    constructor(
-        private router: Router,
-        private authenticationService: AuthenticationService
-    ) {
-        this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
-    }
+  constructor(
+      private router: Router,
+      private authenticationService: AuthenticationService
+  ) {
+      this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+      if (this.authenticationService.currentUserValue) {
+        // 로그인 된 유저가 있는 경우는 일단 1번째 페이지로? (2번째 페이지로 가야할 듯?)
+        this.setStep(1);
+      }
+  }
 
-    logout() {
-        this.authenticationService.logout();
-        this.router.navigate(['/login']);
-    }
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  logout() {
+      this.authenticationService.logout();
+      this.router.navigate(['/login']);
+  }
 }
