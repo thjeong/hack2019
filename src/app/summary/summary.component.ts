a76e45452759f0d68b22de7dd8f74f832375f6d2
Jimmy <xikizima@hotmail.com>
2019-11-21 18:10:30 +0900

    nov 21 end

---
+++ src/app/summary/summary.component.ts
@@ -0,0 +1,75 @@
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AlertService, AuthenticationService } from '@/_services';
+
+@Component({
+    selector: 'summary-cmp',
+    templateUrl: 'summary.component.html'})
+export class SummaryComponent implements OnInit {
+    summaryForm: FormGroup;
+    loading = false;
+    submitted = false;
+    returnUrl: string;
+    
+    @Output() setStepInApp = new EventEmitter<number>();
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private authenticationService: AuthenticationService,
+        private alertService: AlertService
+    ) {
+        // redirect to home if already logged in
+        if (this.authenticationService.currentUserValue) { 
+            this.router.navigate(['/']);
+        }
+    }
+
+    ngOnInit() {
+        this.summaryForm = this.formBuilder.group({
+            earned_income_deduce: ['', Validators.required],
+            personal_allowance: ['', Validators.required],
+            pension_insurance_deduce: ['', Validators.required]
+            //password: ['', Validators.required]
+        });
+
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.summaryForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.summaryForm.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        // TODO: 인증만 넣지말고, 요약 가져오는 것도 여기에 넣을까?
+        this.authenticationService.login(this.f.userid.value) //, this.f.password.value)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.loading = false;
+                    this.setStepInApp.emit(1);
+                    //this.router.navigate([this.returnUrl]);
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
+    }
+
+    logout() {
+        this.authenticationService.logout();
+//        this.router.navigate(['/login']);
+    }
+}
